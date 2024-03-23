aa83b0ec3b714062d4cf746197a63e71bcc29b7d
Anilraj Lokku <anilrajlokku1810@gmail.com>
2024-03-24 00:40:47 +0530

    Operation Cases Added

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -4,6 +4,7 @@
 import { useReducer } from "react";
 import "./styles.css";
 import DigitButton from "./DigitButton";
+import OperationButton from "./OperationButton";
 
 export const ACTIONS = {
  ADD_DIGIT: "add-digit",
@@ -16,13 +17,61 @@ export const ACTIONS = {
 function reducer(state, { type, payload }) {
  switch (type) {
   case ACTIONS.ADD_DIGIT:
+   if (payload.digit === "0" && state.currentOperand === "0") {
+    return state;
+   }
+   if (payload.digit === "." && state.currentOperand.includes(".")) {
+    return state;
+   }
    return {
     ...state,
-    currentOperand: `${state.currentOperand || ""} ${payload.digit}`,
+    currentOperand: `${state.currentOperand || ""}${payload.digit}`,
    };
+  case ACTIONS.CHOOSE_OPERATION:
+   if (state.currentOperand == null && state.previousOperand == null) {
+    return {};
+   }
+   if (state.previousOperand == null) {
+    return {
+     ...state,
+     operation: payload.operation,
+     previousOperand: state.currentOperand,
+     currentOperand: null,
+    };
+   }
+   return {
+    ...state,
+    previousOperand: evaluate(state),
+    operation: payload.operation,
+    currentOperand: null,
+   };
+  case ACTIONS.CLEAR:
+   return state;
  }
 }
 
+function evaluate({ currentOperand, previousOperand, operation }) {
+ const prev = parseFloat(previousOperand);
+ const current = parseFloat(currentOperand);
+ if (isNaN(prev) || isNaN(current)) return "";
+ let computation = "";
+ switch (operation) {
+  case "+":
+   computation = prev + current;
+   break;
+  case "-":
+   computation = prev - current;
+   break;
+  case "*":
+   computation = prev * current;
+   break;
+  case "÷":
+   computation = prev / current;
+   break;
+ }
+
+ return computation.toString();
+}
 function App() {
  //In Calculator the portion consists of two section one is the first  operantion and another one is  the type of operand and 2nd operation is the Current Operation
  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
@@ -40,24 +89,28 @@ function App() {
     </div>
     <div className="current-operand">{currentOperand}</div>
    </div>
-   <button className="span-two">AC</button>
+   <button
+    className="span-two"
+    onClick={() => dispatch({ type: ACTIONS.CLEAR })}
+   >
+    AC
+   </button>
    <button>DEL</button>
-   <DigitButton digit="÷" dispatch={dispatch} />
-   {/* <button>÷</button> */}
-   <button>1</button>
-   <button>2</button>
-   <button>3</button>
-   <button>*</button>
-   <button>4</button>
-   <button>5</button>
-   <button>6</button>
-   <button>+</button>
-   <button>7</button>
-   <button>8</button>
-   <button>9</button>
-   <button>-</button>
-   <button>.</button>
-   <button>0</button>
+   <OperationButton operation="÷" dispatch={dispatch} />
+   <DigitButton digit="1" dispatch={dispatch} />
+   <DigitButton digit="2" dispatch={dispatch} />
+   <DigitButton digit="3" dispatch={dispatch} />
+   <OperationButton operation="*" dispatch={dispatch} />
+   <DigitButton digit="4" dispatch={dispatch} />
+   <DigitButton digit="5" dispatch={dispatch} />
+   <DigitButton digit="6" dispatch={dispatch} />
+   <OperationButton operation="+" dispatch={dispatch} />
+   <DigitButton digit="7" dispatch={dispatch} />
+   <DigitButton digit="8" dispatch={dispatch} />
+   <DigitButton digit="9" dispatch={dispatch} />
+   <OperationButton operation="-" dispatch={dispatch} />
+   <DigitButton digit="." dispatch={dispatch} />
+   <DigitButton digit="0" dispatch={dispatch} />
    <button className="span-two">=</button>
   </div>
  );
